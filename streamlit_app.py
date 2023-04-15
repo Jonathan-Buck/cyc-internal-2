d802cc571bea49aaf9016e03de4c6595378284e0
Sophia Scherer <srschere@ncsu.edu>
2023-04-15 18:25:27 -0400

    Setting up streamlit dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
 #entrypoint for streamlit
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+st.title("CYC Internal Analytics Dashboard")
+
+
+df = pd.read_csv("finals.csv")
+
+print(df)
+
+st.header("Testing Datasets")
+st.subheader("Dataframes")
+st.write("We can display a dataframe that can be manipulated and viewed in the dashboard:")
+st.write(df)
