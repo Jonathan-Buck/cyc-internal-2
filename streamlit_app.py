d664a0722c4067bc34f6aaad76ebda4823f39a50
Sophia Scherer <srschere@ncsu.edu>
2023-04-16 12:32:16 -0400

    Integrate data analytics for historical data into dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,186 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
+#raif's imports
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("trial1.csv")
+df_ag = pd.read_csv("finals.csv")
+
+#DATA CLEANING
+#In Aggregate, W vs. w in recommend with hesitationc:\Users\srsch\Downloads\Dashboard.ipynb
+
+df.Recommend1 = df.iloc[:, 13].str.replace('w', 'W')
+df.Recommend2 = df.iloc[:, 14].str.replace('w', 'W')
+
+df_ag.Recommend1 = df_ag.iloc[:, 14].str.replace('w', 'W')
+df_ag.Recommend2 = df_ag.iloc[:, 15].str.replace('w', 'W')
+
+#Goal: Extract Numeric Values from lists
+scores1_ = []
+scores2_ = []
+for l in df ["Each_Matrix_Num"]:
+    clean = eval(re.sub(r"[\s%']+", "",l))
+    scores1_.append(clean[0])
+    scores2_.append(clean[1])
+
+df = df.assign(Score_one=scores1_)
+df = df.assign(Score_two=scores2_)
+
+
+#Creating Difference Variable
+diffs = abs(df.Score_one - df.Score_two)
+df = df.assign(Score_diff = diffs)
+
+#Goal: Extract Numeric Values from lists in Ag
+scores1_ = []
+scores2_ = []
+for l in df_ag["Each_Matrix_Num"]:
+    if 'nan' in l:
+        scores1_.append(None)
+        scores2_.append(None)
+    else:
+        clean = eval(re.sub(r"[\s%']+", "",l))
+        scores1_.append(clean[0])
+        scores2_.append(clean[1])
+
+df_ag = df_ag.assign(Score_one=scores1_)
+df_ag = df_ag.assign(Score_two=scores2_)
+
+#Creating Difference Variable
+diffs = abs(df_ag.Score_one - df_ag.Score_two)
+df_ag = df_ag.assign(Score_diff = diffs)
+
+#THRESHOLDS
+# select the two columns you want to append vertically
+col1 = df_ag[['Score_one', 'Recommend1']]
+col2 = df_ag[['Score_two', 'Recommend2']]
+
+col1 = col1.rename(columns={"Score_one": "Score", "Recommend1": "Recommendation"})
+col2 = col2.rename(columns={"Score_two": "Score", "Recommend2": "Recommendation"})
+
+
+# create a new dataframe by appending the two columns together vertically
+appended_scores = pd.concat([col1, col2], axis=0)
+
+# reset the index of the new dataframe
+appended_scores = appended_scores.reset_index(drop=True)
+
+# select the two columns you want to append vertically
+col1 = df[['Score_one', 'Recommend1']]
+col2 = df[['Score_two', 'Recommend2']]
+
+col1 = col1.rename(columns={"Score_one": "Score", "Recommend1": "Recommendation"})
+col2 = col2.rename(columns={"Score_two": "Score", "Recommend2": "Recommendation"})
+
+
+# create a new dataframe by appending the two columns together vertically
+appended_scores_single = pd.concat([col1, col2], axis=0)
+
+# reset the index of the new dataframe
+appended_scores_single = appended_scores_single.reset_index(drop=True)
+#appended_scores_single
+
+# sort the dataframe by category
+appended_scores_sorted = appended_scores.sort_values(by=['Recommendation'])
+
+# report descriptive statistics for the 'value' column, grouped by category
+stats = appended_scores_sorted.groupby('Recommendation')['Score'].describe()
+
+# sort the dataframe by category
+appended_scores_single_sorted = appended_scores_single.sort_values(by=['Recommendation'])
+
+# report descriptive statistics for the 'value' column, grouped by category
+stats = appended_scores_single_sorted.groupby('Recommendation')['Score'].describe()
+
+appended_nonzero = appended_scores_single_sorted.replace(0, np.nan)
+appended_nonzero = appended_nonzero.dropna(how='all', axis=0)
+
+appended_nonzero2 = appended_scores_sorted.replace(0, np.nan)
+appended_nonzero2 = appended_nonzero2.dropna(how='all', axis=0)
+
+
+#DISCREPANCIES
+
+#Outlier Detection
+
+#Defining New Variable for Discrepancy between Recommendation Decisions
+for index, row in df_ag.iterrows():
+    if row['Recommend1'] != row['Recommend2']:
+        df_ag.at[index, 'Agreement'] = 'Discrepancy'
+    else:
+        df_ag.at[index, 'Agreement'] = 'No Discrepancy'
+
+for index, row in df.iterrows():
+    if row['Recommend1'] != row['Recommend2']:
+        df.at[index, 'Agreement'] = 'Discrepancy'
+    else:
+        df.at[index, 'Agreement'] = 'No Discrepancy'
+
+#Creating outlier and flag values      
+df_ag_flag = df_ag[df_ag['Score_diff']> 10].copy()
+df_ag_outlier = df_ag[df_ag['Score_diff'] > 30].copy()
+
+df_flag = df[df['Score_diff']> 10].copy()
+df_outlier = df[df['Score_diff'] > 30].copy()
+
+#Look into Abisha Finn for max
+
+
+
+
+
 
 st.title("CYC Internal Analytics Dashboard")
 
 
-df = pd.read_csv("finals.csv")
+
 
 
 st.header("Threshold Insights")
 
 
+thresh1 = sns.displot(appended_nonzero, x="Score", hue="Recommendation", kind="ecdf")
+st.pyplot(thresh1)
+
+thresh2 = sns.displot(appended_nonzero2, x="Score", hue="Recommendation", kind="ecdf")
+st.pyplot(thresh2)
+
+
+
 st.header("Discrepancy Analysis")
+
+st.write("Flagged interviews")
+st.write(df_flag)
+
+st.write("Aggregate flagged interviews")
+st.write(df_ag_flag)
+
+
+fig, axes = plt.subplots(2, 2, figsize=(20, 10))
+fig.suptitle('Score Differences and Discrepancies')
+
+#Fall 2022
+sns.kdeplot(ax=axes[0,0], data=df, x="Score_diff", hue="Agreement")
+axes[0,0].set_title('Fall 2022')
+
+#Fall 2022 Flagged
+sns.kdeplot(ax=axes[0,1], data=df_flag, x="Score_diff", hue="Agreement")
+axes[0,1].set_title('Fall 2022 Flagged')
+
+#Aggregate
+sns.kdeplot(ax=axes[1,0], data=df_ag, x="Score_diff", hue="Agreement")
+axes[1,0].set_title('Aggregate')
+
+#Aggregate Flagged
+sns.kdeplot(ax=axes[1,1], data=df_ag_flag, x="Score_diff", hue="Agreement")
+axes[1,1].set_title('Aggregate Flagged')
+
+st.write(fig)
+
+
 
 
 st.header("Impact of GPA")
@@ -28,16 +197,16 @@ black = 0
 asian = 0
 hispanic = 0
 
-for entries in range(0, len(df)):
-    if(df.iloc[entries,10] == "I do not wish to disclose."):
+for entries in range(0, len(df_ag)):
+    if(df_ag.iloc[entries,10] == "I do not wish to disclose."):
         unknown += 1
-    elif(df.iloc[entries,10] == "Asian: A person having origins in any of the original peoples of the Far East, Southeast Asia or the Indian Subcontinent, including, for example, Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand and Vietnam."):
+    elif(df_ag.iloc[entries,10] == "Asian: A person having origins in any of the original peoples of the Far East, Southeast Asia or the Indian Subcontinent, including, for example, Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand and Vietnam."):
         asian += 1
-    elif(df.iloc[entries,10] == "White: A person having origins in any of the original peoples of Europe, the Middle East or North Africa."):
+    elif(df_ag.iloc[entries,10] == "White: A person having origins in any of the original peoples of Europe, the Middle East or North Africa."):
         white += 1
-    elif(df.iloc[entries,10] == "Black or African American: A person having origins in any of the black racial groups of Africa."):
+    elif(df_ag.iloc[entries,10] == "Black or African American: A person having origins in any of the black racial groups of Africa."):
         black += 1
-    elif(df.iloc[entries,10] == "Hispanic or Latino: A person of Cuban, Mexican, Puerto Rican, South or Central American, or other Spanish culture or origin regardless of race."):
+    elif(df_ag.iloc[entries,10] == "Hispanic or Latino: A person of Cuban, Mexican, Puerto Rican, South or Central American, or other Spanish culture or origin regardless of race."):
         hispanic += 1
 
 
